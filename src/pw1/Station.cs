f00ef228c9c94dbe47fb177be31e62465b082e4b
angela <9302227@alumnos.ufv.es>
2025-06-09 15:31:24 +0200

    solved file problem (only loads it with the absolute one tho)

--- src/pw1/Station.cs
+++ src/pw1/Station.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TrainStation
 {
@@ -29,15 +30,140 @@ namespace TrainStation
 
         public bool LoadFromFile(string path)
         {
-            if (!File.Exists(path))
-            {
-                Console.WriteLine("File does not exist");
-                return false;
-            }
+            string id = "";
+            int arrivalTime = 0;
+            string type = "";
+            int numberOfPassengers = 0;
+            int capacity = 0;
+            int maxWeight = 0;
+            string freightType = "";
+
+            bool validAccess = true;
 
             try
             {
-                string[] lines = File.ReadAllLines(path);
+                if (File.Exists(path))
+                {
+                    StreamReader sr = File.OpenText(path);
+                    string? line = sr.ReadLine();
+                    line = sr.ReadLine();
+
+                    while (line != null)
+                    {
+                        try
+                        {
+                            //id,arrivalTime,type,additionalData1,additionalData2
+                            string[] parts = line.Split(',');
+                            if (parts.Length < 5)
+                            {
+                                Console.WriteLine("File Format Error.");
+                                Console.ReadLine();
+                            }
+                            else
+                            {
+                                //id
+                                try
+                                {
+                                    id = parts[0];
+                                }
+                                catch
+                                {
+                                    Console.WriteLine("Invalid ID format.");
+                                }
+
+                                //arrival time
+                                try
+                                {
+                                    arrivalTime = Convert.ToInt32(parts[1]);
+                                }
+                                catch
+                                {
+                                    Console.WriteLine("Invalid distance format.");
+                                    Console.ReadLine();
+                                }
+
+                                //type
+                                if (type == "Passengers")
+                                {
+                                    try
+                                    {
+                                        numberOfPassengers = int.Parse(parts[3]);
+                                        capacity = int.Parse(parts[4]);
+
+                                        Trains.Add(new PassengerTrain(id, arrivalTime, type, numberOfPassengers, capacity));
+                                    }
+                                    catch
+                                    {
+                                        Console.WriteLine("Invalid Additional Data format.");
+                                        Console.ReadLine();
+                                    }
+                                }
+                                if (type == "Freight")
+                                {
+                                    try
+                                    {
+                                        maxWeight = int.Parse(parts[3]);
+                                        freightType = parts[4];
+
+                                        Trains.Add(new FreightTrain(id, arrivalTime, type, maxWeight, freightType));
+                                    }
+                                    catch
+                                    {
+                                        Console.WriteLine("Invalid Additional Data format.");
+                                        Console.ReadLine();
+                                    }
+                                }
+
+                            }
+                        }
+                        catch (ArgumentNullException)
+                        {
+                            Console.WriteLine("Error, please enter the correct data type, not null.");
+                            Console.ReadLine();
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("Error, please enter the correct data type.");
+                            Console.ReadLine();
+                        }
+
+                        line = sr.ReadLine(); //read the next line of the file 
+                    }
+
+                    sr.Close(); //close the file after reading it
+                }
+                else
+                {
+                    Console.WriteLine("File not found.");
+                    Console.ReadLine();
+                    validAccess = false;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error loading file: " + ex.Message);
+                Console.ReadLine();
+                validAccess = false;
+            }
+            return validAccess;
+        }
+
+
+        public void StartSimulation()
+        {
+            Console.WriteLine("start simulation");
+        }
+
+    }
+}
+
+
+
+/*
+
+
+
+                    string[] lines = File.ReadAllLines(path);
 
                 foreach (string line in lines)
                 {
@@ -48,22 +174,22 @@ namespace TrainStation
                         Console.WriteLine("Error");
                         return false;
                     }
-                    string id = parts[0];
-                    int arrivalTime = int.Parse(parts[1]);
-                    string type = parts[2];
+                    id = parts[0];
+                    arrivalTime = int.Parse(parts[1]);
+                    type = parts[2];
 
                     if (type.Equals("Passengers", StringComparison.OrdinalIgnoreCase))
                     {
-                        int numberOfPassengers = int.Parse(parts[3]);
-                        int capacity = int.Parse(parts[4]);
+                        numberOfPassengers = int.Parse(parts[3]);
+                        capacity = int.Parse(parts[4]);
 
                         Trains.Add(new PassengerTrain(id, arrivalTime, type, numberOfPassengers, capacity));
                     }
 
                     if (type.Equals("Freight", StringComparison.OrdinalIgnoreCase))
                     {
-                        int maxWeight = int.Parse(parts[3]);
-                        string freightType = parts[4];
+                        maxWeight = int.Parse(parts[3]);
+                        freightType = parts[4];
 
                         Trains.Add(new FreightTrain(id, arrivalTime, type, maxWeight, freightType));
                     }
@@ -82,11 +208,5 @@ namespace TrainStation
             }
             return true;
         }
-
-        public void StartSimulation()
-        {
-
-        }
-
-    }
-}
+        
+*/
